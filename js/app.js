be6d0dc29d6281a71c540b2808236f6f6daa6247
David Fisher <fisherds@rose-hulman.edu>
2015-08-31 13:03:02 -0400

    auto indent

--- js/app.js
+++ js/app.js
@@ -1,76 +1,76 @@
 (function() {
-  var app = angular.module("moviequotes", [ "ui.bootstrap", "modal-controllers", "firebase" ]);
+    var app = angular.module("moviequotes", [ "ui.bootstrap", "modal-controllers", "firebase" ]);
 
-  app.controller("MoviequotesCtrl", function($modal, $firebaseArray) {
-    this.navbarCollapsed = true;
-    var _this = this;
-    //Done: Bind data to Firebase
-    var moviequotesRef = new Firebase("https://fisherds-auth-movie-quotes.firebaseio.com/quotes");
-    this.items = $firebaseArray(moviequotesRef);
+    app.controller("MoviequotesCtrl", function($modal, $firebaseArray) {
+        this.navbarCollapsed = true;
+        var _this = this;
+        //Done: Bind data to Firebase
+        var moviequotesRef = new Firebase("https://fisherds-auth-movie-quotes.firebaseio.com/quotes");
+        this.items = $firebaseArray(moviequotesRef);
 
-    var compare = function(a, b) {
-      return a.$id < b.$id;
-    }
-    this.items.$watch(function() { _this.items.sort(compare); });
-
-  this.showAddQuoteDialog = function(movieQuoteFromRow) {
-    this.navbarCollapsed = true;
-    var modalInstance = $modal.open({
-      templateUrl : "/partials/addQuoteModal.html",
-      controller : "AddQuoteModalCtrl",
-      controllerAs : "insertModal"
-    });
-    modalInstance.result.then(function(movieQuoteFromModal) {
-      //Done: Add movieQuote to Firebase
-      _this.items.$add(movieQuoteFromModal);
-      _this.isEditing = false;
-    });
-  };
-
-    this.showUpdateQuoteDialog = function(movieQuoteFromRow) {
-      this.navbarCollapsed = true;
-      var modalInstance = $modal.open({
-        templateUrl : "/partials/updateQuoteModal.html",
-        controller : "UpdateQuoteModalCtrl",
-        controllerAs : "insertModal",
-        resolve : {
-          movieQuote : function() {
-            return {
-              get :
-                function() {
-                  return movieQuoteFromRow;
-              },
-              save :
-                function(movieQuoteFromModal) {
-                  //Done: save movieQuote to Firebase
-                  _this.items.$save(movieQuoteFromModal);
-              }
-            };
-          }
+        var compare = function(a, b) {
+            return a.$id < b.$id;
         }
-      });
+        this.items.$watch(function() { _this.items.sort(compare); });
 
-      modalInstance.result.then(function() {
-        _this.isEditing = false;
-      });
-    };
+        this.showAddQuoteDialog = function(movieQuoteFromRow) {
+            this.navbarCollapsed = true;
+            var modalInstance = $modal.open({
+                templateUrl : "/partials/addQuoteModal.html",
+                controller : "AddQuoteModalCtrl",
+                controllerAs : "insertModal"
+            });
+            modalInstance.result.then(function(movieQuoteFromModal) {
+                //Done: Add movieQuote to Firebase
+                _this.items.$add(movieQuoteFromModal);
+                _this.isEditing = false;
+            });
+        };
 
-    this.showDeleteQuoteDialog = function(movieQuoteFromRow) {
-      var modalInstance = $modal.open({
-        templateUrl : "/partials/deleteQuoteModal.html",
-        controller : "DeleteQuoteModalCtrl",
-        controllerAs : "deleteModal",
-        resolve : {
-          movieQuoteInModal : function() {
-            return movieQuoteFromRow;
-          }
-        }
-      });
-      modalInstance.result.then(function(movieQuoteFromModal) {
-        //Done: Delete the moviequote from Firebase
-        _this.items.$remove(movieQuoteFromModal);
-        _this.isEditing = false;
-      });
-    };
-  });
+        this.showUpdateQuoteDialog = function(movieQuoteFromRow) {
+            this.navbarCollapsed = true;
+            var modalInstance = $modal.open({
+                templateUrl : "/partials/updateQuoteModal.html",
+                controller : "UpdateQuoteModalCtrl",
+                controllerAs : "insertModal",
+                resolve : {
+                    movieQuote : function() {
+                        return {
+                            get :
+                            function() {
+                                return movieQuoteFromRow;
+                            },
+                            save :
+                            function(movieQuoteFromModal) {
+                                //Done: save movieQuote to Firebase
+                                _this.items.$save(movieQuoteFromModal);
+                            }
+                        };
+                    }
+                }
+            });
+
+            modalInstance.result.then(function() {
+                _this.isEditing = false;
+            });
+        };
+
+        this.showDeleteQuoteDialog = function(movieQuoteFromRow) {
+            var modalInstance = $modal.open({
+                templateUrl : "/partials/deleteQuoteModal.html",
+                controller : "DeleteQuoteModalCtrl",
+                controllerAs : "deleteModal",
+                resolve : {
+                    movieQuoteInModal : function() {
+                        return movieQuoteFromRow;
+                    }
+                }
+            });
+            modalInstance.result.then(function(movieQuoteFromModal) {
+                //Done: Delete the moviequote from Firebase
+                _this.items.$remove(movieQuoteFromModal);
+                _this.isEditing = false;
+            });
+        };
+    });
 })();
